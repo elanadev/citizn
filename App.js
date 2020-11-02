82dbe702fec948f27a49b9815ecee714e92f39c1
Elana Cohen-Khani <elanadev@gmail.com>
2020-11-01 20:07:51 -0800

    iOS simulator works

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{
+          backgroundColor: 'black',
+        }}>
+        <Text style={{ fontSize: 20, color: '#fff'}}>Click me to go to the next page</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
