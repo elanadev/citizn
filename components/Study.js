536062d4cf5a21e032e6296dd96f6e318955587f
Elana Cohen-Khani <elanadev@gmail.com>
2020-11-03 21:41:25 -0800

    Audio files working

--- components/Study.js
+++ components/Study.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { Text, View, ScrollView, SafeAreaView, ImageBackground, StyleSheet, FlatList } from 'react-native';
 
 import Carousel from 'react-native-snap-carousel';
-import Blackboard from '../assets/blackboard.jpeg'
+import Blackboard from '../assets/blackboard.jpeg';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Audio } from 'expo-av';
+
 
 class Study extends React.Component {
 
@@ -19,21 +22,59 @@ class Study extends React.Component {
       str = str[0].toUpperCase() + str.slice(1)
       return '📜  ' + str
     })
+
     const text = capped.join('\n')
+
+    const leadingZero = (num) => { if (num < 10) { return `0${num}` } else { return `${num}` } }
+    const audioUrl = `https://www.uscis.gov/sites/default/files/document/audio/Track%20${leadingZero(index + 1)}.mp3`
+
+    const soundObject = new Audio.Sound();
+    const playAudio = async (url) => {
+      try {
+        await soundObject.loadAsync({ uri: url });
+        soundObject.setOnPlaybackStatusUpdate(async (status) => {
+          if (status.didJustFinish === true) {
+            // audio has finished!
+            await soundObject.unloadAsync()
+            console.log('unloaded')
+          }
+        })
+
+        await soundObject.playAsync();
+
+        // Your sound is playing!
+
+        // Don't forget to unload the sound from memory
+        // when you are done using the Sound object
+        console.log('hello from play audio')
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
     return (
       <ScrollView style={{
-        backgroundColor: 'ghostwhite',
+        backgroundColor: 'whitesmoke',
         borderRadius: 10,
         height: 600,
         padding: 40,
         marginLeft: 25,
         marginRight: 25,
-        textShadowColor: 'white', textShadowOffset: { width: 0.3, height: 0.3 }, textShadowRadius: 1
+        textShadowColor: 'blue', textShadowOffset: { width: 0.3, height: 0.3 }, textShadowRadius: 1
       }}>
+        <TouchableOpacity
+          onPress={() => playAudio(audioUrl)}
+          style={{
+            backgroundColor: 'transaparent',
+            paddingVertical: 0,
+            alignSelf: 'flex-end',
+          }}
+        >
+          <Text style={{ fontSize: 20, color: '#fff' }}>🔊</Text>
+        </TouchableOpacity>
         <Text style={{ fontSize: 12 }}>{item.heading}</Text>
         <Text style={{ fontSize: 12 }}>{item.subheading}</Text>
-        <Text style={{ fontSize: 24, marginVertical: 20 }}>{item.question}</Text>
+        <Text style={{ fontSize: 24, marginVertical: 20, textShadowColor: 'white', textShadowOffset: { width: 0.3, height: 0.3 }, textShadowRadius: 1 }}>{item.question}</Text>
         <Text style={{ fontSize: 16, lineHeight: 32 }}>{text}</Text>
       </ScrollView>
     )
@@ -45,11 +86,11 @@ class Study extends React.Component {
     return (
       <SafeAreaView style={{ flex: 1, paddingTop: 50, }}>
         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-
           <ImageBackground source={Blackboard} style={{ flex: 1 }}>
+
             <View style={{ marginTop: 50 }}>
               <Carousel
-                layout={"tinder"}
+                layout={"stack"}
                 ref={ref => this.carousel = ref}
                 data={questions}
                 sliderWidth={400}
@@ -59,7 +100,7 @@ class Study extends React.Component {
             </View>
           </ImageBackground>
         </View>
-      </SafeAreaView >
+      </SafeAreaView>
     );
   }
 }
--- components/Welcome.js
+++ components/Welcome.js
@@ -1,44 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Pressable, ImageBackground } from 'react-native';
+import Blackboard from '../assets/blackboard.jpeg';
+import { Autocomplete } from 'react-native-autocomplete-input';
 
 import db from '../firebase.config';
+
+const styles = StyleSheet.create({
+  button: {
+    borderColor: '#00001a',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    paddingVertical: 50,
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#00001a'
+  }
+})
 
 const Welcome = ({ navigation }) => (
   <SafeAreaView style={{ flex: 1 }}>
     <Text> What would you like to do?</Text>
 
-    <View style={{ flex: 1 }}>
-      <TouchableOpacity
+    <View style={{ flex: 1, justifyContent: 'space-evenly', marginVertical: 70 }}>
+      <Pressable
         onPress={() => navigation.navigate('Study', { name: 'Study' })}
-        style={{
-          backgroundColor: 'black',
-          paddingVertical: 50,
-          alignItems: 'center',
-        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'red',
+          },
+          styles.button,
+        ]}
       >
-        <Text style={{ fontSize: 20, color: '#fff' }}>Study</Text>
-      </TouchableOpacity>
+        <Text style={styles.buttonText}>Study</Text>
+        <ImageBackground source={Blackboard} style={{ flex: 1 }}></ImageBackground>
+      </Pressable>
 
-      <TouchableOpacity
+      <Pressable
         onPress={() => navigation.navigate('Practice', { name: 'Practice' })}
-        style={{
-          backgroundColor: 'blue',
-          paddingVertical: 50,
-          alignItems: 'center',
-        }}
-      >
-        <Text style={{ fontSize: 20, color: '#fff' }}>Practice</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
+        style={styles.button}>
+        <Text style={styles.buttonText}>Practice</Text>
+      </Pressable>
+
+      <Pressable
         onPress={() => navigation.navigate('Quiz', { name: 'Quiz' })}
-        style={{
-          backgroundColor: 'red',
-          paddingVertical: 50,
-          alignItems: 'center',
-        }}
-      >
-        <Text style={{ fontSize: 20, color: '#fff' }}>Quiz</Text>
-      </TouchableOpacity>
+        style={styles.button}>
+        <Text style={styles.buttonText}>Quiz</Text>
+      </Pressable>
+
     </View>
   </SafeAreaView>
 );
